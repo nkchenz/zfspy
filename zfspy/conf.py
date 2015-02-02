5673aedd1a839b868bac0a04efc30e013dddaa48
Jerome Lebel <lebel.jerome@gmail.com>
2015-02-02 09:21:30 +0100

    support for FreeBSD

--- zfspy/conf.py
+++ zfspy/conf.py
@@ -1,3 +1,8 @@
-ZPOOL_CACHE = '/etc/zfs/zpool.cache'
+import platform
+
+if platform.system() == "FreeBSD":
+    ZPOOL_CACHE = '/boot/zfs/zpool.cache'
+else:
+    ZPOOL_CACHE = '/etc/zfs/zpool.cache'
 
 debug = False
